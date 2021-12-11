3588d1409a229c5c61ef958ce941780b84808042
AnnaABogatyreva <87075158+AnnaABogatyreva@users.noreply.github.com>
2021-12-11 18:20:33 +0300

    task20 completed

---
+++ src/ru/mirea/task20/NeNaMeli/src/base/AdminAPI.java
@@ -0,0 +1,46 @@
+package base;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class AdminAPI {
+    private DataBase db;
+    private List<String> history = new ArrayList<>();
+    private Date login;
+
+    public AdminAPI(DataBase db, List<String> history, Date login) {
+        this.db = db;
+        this.history = history;
+        this.login = login;
+        history.add("login at: " + login);
+    }
+
+    public AdminAPI(DataBase db, Date login) {
+        this.db = db;
+        this.login = login;
+        history.add("login at: " + login);
+    }
+
+    public List<String> getHistory() {
+        return history;
+    }
+
+    private void notifyUser(BankUser toNotify, String notification){
+        toNotify.notify = notification;
+        history.add("notified " + toNotify + ", msg: " + notification);
+    }
+
+    private List<Account> getUserAccounts(BankUser user) throws NoSuchUserException {
+        if (user == null)
+            throw new NoSuchUserException();
+        List<Account> accs = db.getOpenedAccounts().get(user);
+        if (accs == null)
+            throw new NoSuchUserException();
+
+        history.add("Список аккаунтов " + user +" был запрошен. Время: " +
+                LocalDateTime.now());
+        return db.getOpenedAccounts().get(user);
+    }
+}
